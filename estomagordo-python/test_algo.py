efd259a3f020c28fbfec40191ad7549afaa4a9e6
Christofer Ohlsson <christofer@miro.com>
2023-11-21 19:27:24 +0100

    testing how generic sssp is

--- estomagordo-python/test_algo.py
+++ estomagordo-python/test_algo.py
@@ -19,3 +19,23 @@ def test_sssp_greediness():
     shortest_path = sssp(graph, start, goal_function_is_100, step_finder)
 
     assert(52 == shortest_path)
+
+
+def test_sssp_three_dimensional():
+    def goal_function_is_100(position):
+        return position == (1, 1, 1)
+    
+    def step_finder(graph, position):
+        return graph[position]
+    
+    graph = {
+        (0, 0, 0): ((1, (0, 1, 0)), (2, (1, 0, 1))),
+        (0, 1, 0): ((100, (1, 1, 1)),),
+        (1, 0, 1): ((50, (1, 1, 1)),),
+    }
+
+    start = (0, 0, 0)
+
+    shortest_path = sssp(graph, start, goal_function_is_100, step_finder)
+
+    assert(52 == shortest_path)
