263ffa23ed2cbc56b373700f74f63f7ca578be56
Christofer Ohlsson <christofer@miro.com>
2023-11-21 20:10:41 +0100

    only one program file per day this year

--- estomagordo-python/initializer.py
+++ estomagordo-python/initializer.py
@@ -1,15 +1,19 @@
 from os import path
 from sys import argv
 
-dayfile = lambda day: f"""from collections import Counter, defaultdict, deque
+program_file = lambda day: f"""from collections import Counter, defaultdict, deque
 from functools import cache, reduce
 from heapq import heapify, heappop, heappush
 from itertools import combinations, permutations, product
 from helpers import adjacent, chunks, chunks_with_overlap, columns, custsort, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, positives, rays, rays_from_inside
 
 
-def solve(lines):
-    pass
+def solve_a(lines):
+    return None
+
+
+def solve_b(lines):
+    return None
 
 
 def main():
@@ -19,26 +23,23 @@ def main():
         for line in f.readlines():
             lines.append(line)
             
-    return solve(lines)
+    print(solve_a(lines))
+    print(solve_b(lines))
 
 
 if __name__ == '__main__':
-    print(main())
+    main()
 """
 
 if __name__ == '__main__':
     day = argv[1]
     
-    daya = f'{day}a.py'
-    dayb = f'{day}b.py'
+    program = f'{day}.py'
     inp = f'{day}.txt'
 
-    if not path.isfile(daya):
-        with open(daya, 'w') as g:
-            g.write(dayfile(day))
-    if not path.isfile(dayb):
-        with open(dayb, 'w') as g:
-            g.write(dayfile(day))
+    if not path.isfile(program):
+        with open(program, 'w') as g:
+            g.write(program_file(day))
     if not path.isfile(inp):
         with open(inp, 'w') as g:
             g.write('')
