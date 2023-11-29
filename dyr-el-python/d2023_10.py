ad56f22265b046f3af4abecdac615fc85ef638c8
Erik Dyrelius <erik.dyrelius@scania.com>
2023-11-29 11:31:33 +0100

    Generate day templates

---
+++ dyr-el-python/d2023_10.py
@@ -0,0 +1,27 @@
+from aoc_prepare import PrepareAoc
+
+
+def part1(inp):
+    pass
+
+
+def part2(inp):
+    pass
+
+
+def test_1_1():
+    pass
+
+
+def test_1_2():
+    pass
+
+
+def main(inp):
+    print("Part1:", part1(inp.strip()))
+    print("Part2:", part2(inp.strip()))
+
+
+if __name__ == "__main__":
+    prep = PrepareAoc(2023, 10)
+    main(prep.get_content())
