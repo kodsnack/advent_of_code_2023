eb290d817979f0b9f1f1c19ed0852e838b875ca6
Christofer Ohlsson <christofer@miro.com>
2023-12-04 06:23:20 +0100

    bit cleaner 4

--- estomagordo-python/4.py
+++ estomagordo-python/4.py
@@ -7,35 +7,24 @@ from algo import a_star, custsort, sssp
 from helpers import adjacent, chunks, chunks_with_overlap, columns, digits, distance, distance_sq, eight_neighs, eight_neighs_bounded, grouped_lines, ints, manhattan, multall, n_neighs, neighs, neighs_bounded, positives, rays, rays_from_inside
 
 
-def solve_a(lines):
-    score = 0
-
+def parse(lines):
     for line in lines:
         a, b = line.split('|')
 
         winners = set(ints(a.split(':')[1]))
         mine = set(ints(b))
 
-        matches = len(winners&mine)
+        yield len(winners&mine)
 
-        if matches:
-            score += 2**(matches-1)
 
-    return score
+def solve_a(lines):
+    return sum(int(2**(w-1))for w in parse(lines))
 
 
 def solve_b(lines):
     n = len(lines)
     cards = [1 for _ in range(n)]
-    wins = []
-
-    for line in lines:
-        a, b = line.split('|')
-
-        winners = set(ints(a.split(':')[1]))
-        mine = set(ints(b))
-
-        wins.append(len(winners&mine))
+    wins = list(parse(lines))
         
     for i in range(n):
         c = cards[i]
@@ -46,7 +35,7 @@ def solve_b(lines):
                 cards[x] += c
             else:
                 break
-    print(cards)
+            
     return sum(cards)
 
 
