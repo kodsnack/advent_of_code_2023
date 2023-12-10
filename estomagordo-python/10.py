788b078fb8fa7dcdfa585ff25a4946fd43789212
Christofer Ohlsson <christofer@miro.com>
2023-12-10 07:33:58 +0100

    trying quite hard here

--- estomagordo-python/10.py
+++ estomagordo-python/10.py
@@ -80,21 +80,83 @@ def solve_a(lines):
     return max(seen.values())
 
 
-def solve_b(lines):
-    sy, sx = parse(lines)
+def solve_b(lines):    
     h = len(lines)
     w = len(lines[0])
 
+    origh = h
+    origw = w
+
+    ystretchgraph = [[('.', -1, -1) for _ in range(w)]]
+
+    for y, line in enumerate(lines):
+        stretchrow = []
+
+        for x, c in enumerate(line):
+            if c in '|LJ':
+                stretchrow.append(('|', -1, -1))
+            else:
+                stretchrow.append(('.', -1, -1))        
+
+        ystretchgraph.append(stretchrow)
+
+        row = []
+
+        for x, c in enumerate(line):
+            row.append((c, y, x))
+
+        ystretchgraph.append(row)
+
+    ystretchgraph.append([('.', -1, -1) for _ in range(w)])
+
+    fullgraph = []
+
+    for row in ystretchgraph:
+        newrow = [['.', -1, -1]]
+
+        for c, y, x in row:
+            if c in '-J7':
+                newrow.append(['-', -1, -1])
+            else:
+                newrow.append([c, y, x])
+
+        newrow.append(['.', -1, -1])
+        
+        fullgraph.append(newrow)
+
+    h = len(fullgraph)
+    w = len(fullgraph[0])
+
+    for y, row in enumerate(fullgraph):
+        for x, tile in enumerate(row):
+            if x > 0 and fullgraph[y][x-1][0] == '-' and y < h-1 and fullgraph[y+1][x][0] == '|':
+                fullgraph[y][x][0] = '7'
+            if x > 0 and fullgraph[y][x-1][0] == '-' and y > 0 and fullgraph[y-1][x][0] == '|':
+                fullgraph[y][x][0] = 'J'
+    
+    for y, row in enumerate(fullgraph):
+        print(''.join(r[0] for r in row))  
+
+    sy = -1
+    sx = -1
+
+    for y, row in enumerate(fullgraph):
+        for x, tile in enumerate(row):
+            if tile[0] == 'S':
+                sy = y
+                sx = x
+
     seen = {(sy, sx): 0}
+    loopseen = {(sy, sx)}
     frontier = []
 
-    if sy > 0 and lines[sy-1][sx] in '|7F':
+    if sy > 0 and fullgraph[sy-1][sx][0] in '|7F':
         frontier.append((1, sy-1, sx))        
-    if sy < h-1 and lines[sy+1][sx] in '|LJ':
+    if sy < h-1 and fullgraph[sy+1][sx][0] in '|LJ':
         frontier.append((1, sy+1, sx))
-    if sx > 0 and lines[sy][sx-1] in '-LF':
+    if sx > 0 and fullgraph[sy][sx-1][0] in '-LF':
         frontier.append((1, sy, sx+1))        
-    if sx < w-1 and lines[sy][sx+1] in '-J7':
+    if sx < w-1 and fullgraph[sy][sx+1][0] in '-J7':
         frontier.append((1, sy, sx+1))
 
     for steps, y, x in frontier:
@@ -103,7 +165,12 @@ def solve_b(lines):
 
         seen[(y, x)] = steps
 
-        c = lines[y][x]
+        origy, origx = fullgraph[y][x][1:]
+        
+        if origy > -1 and origx > -1:
+            loopseen.add((origy, origx))
+
+        c = fullgraph[y][x][0]
         cangoup = y > 0
         cangodown = y < h - 1
         cangoleft = x > 0
@@ -141,20 +208,31 @@ def solve_b(lines):
                 if cangoright and (y, x+1) not in seen:
                     frontier.append((steps+1, y, x+1))
 
+    print()
+    
+    for y in range(origh):
+        row = []
+
+        for x in range(origw):
+            row.append('*' if (y, x) in loopseen else '.')
+
+        print(row)
+    
     frontier = []
 
     for y in range(h):
-        if lines[y][0] == '.':
+        if fullgraph[y][0][0] == '.':
             frontier.append((y, 0))
-        if lines[y][w-1] == '.':
+        if fullgraph[y][w-1][0] == '.':
             frontier.append((y, w-1))
 
     for x in range(w):
-        if lines[0][x] == '.':
+        if fullgraph[0][x][0] == '.':
             frontier.append((0, x))
-        if lines[h-1][x] == '.':
+        if fullgraph[h-1][x][0] == '.':
             frontier.append((h-1, x))
-
+    
+    genuine_outside = set()    
     outside = set()
 
     for y, x in frontier:
@@ -163,13 +241,18 @@ def solve_b(lines):
 
         outside.add((y, x))
 
+        ry, rx = fullgraph[y][x][1:]
+
+        if ry > -1 and rx > -1:
+            genuine_outside.add((ry, rx))
+
         for ny, nx in neighs_bounded(y, x, 0, h-1, 0, w-1):
             if (ny, nx) in outside or (ny, nx) in seen:
                 continue
 
             frontier.append((ny, nx))
 
-    return h*w - len(seen) - len(outside)
+    return origh * origw - len(loopseen) - len(genuine_outside)
 
 
 def main():
